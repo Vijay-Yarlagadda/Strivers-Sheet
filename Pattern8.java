7adb24117d11fe92db6f543b6b9343334fa6f060
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-09 15:52:00 +0530

    patterns

---
+++ Pattern8.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Pattern8 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter no of rows:");
+        n = sc.nextInt();
+        int i, j;
+        for (i = 0; i < n; i++) {
+            // spaces
+            for (j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (j = 0; j < i; j++) {
+                System.out.print("");
+            }
+            System.out.println();
+        }
+    }
+}
