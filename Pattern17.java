1c5d0a0920587b8e5803db19c97c7f5f256dd3f9
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-10 14:39:46 +0530

    patterns

---
+++ Pattern17.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Pattern17 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows: ");
+        int n = sc.nextInt();
+        int i, j;
+        System.out.println("Pattern is:");
+        for (i = 0; i < n; i++) {
+            // spaces
+            for (j = 0; j < n - 1 - i; j++) {
+                System.out.print(" " + " ");
+            }
+            // letters
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+            for (j = 1; j <= 2 * i + 1; j++) {
+                System.out.print(ch + " ");
+                if (j <= breakpoint) {
+                    ch++;
+                } else {
+                    ch--;
+                }
+            }
+            // spaces
+            for (j = 0; j < n - 1 - i; j++) {
+                System.out.print(" " + " ");
+            }
+            System.out.println();
+        }
+    }
+}
