513e312da79fb737f5c56a376df27abf2d053a2a
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-10 14:53:21 +0530

    patterns

---
+++ Pattern18.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Pattern18 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows: ");
+        int n = sc.nextInt();
+        int i, j;
+        System.out.println("Pattern is:");
+        for (i = 0; i < n; i++) {
+            // alphabets
+            char start = (char) ('A' + n - 1);
+            for (j = 0; j <= i; j++) {
+                System.out.print(start + " ");
+                start--;
+            }
+            System.out.println();
+        }
+    }
+}
