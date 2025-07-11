787435d3e08906256a193d98097ec243c51f3f46
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-11 16:39:31 +0530

    digits

--- Digits-Concept/Armstrong.java
+++ Digits-Concept/Armstrong.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
-class Armstrong{
-	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
+class Armstrong {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number: ");
-		int n=sc.nextInt();
-		int digit,num=0,temp=n;
-		while(n>0){
-			digit=n%10;
-			num=num+(digit*digit*digit);
-			n=n/10;
+		int n = sc.nextInt();
+		int digit, num = 0;
+		int temp = n;
+		int count = String.valueOf(n).length();
+		while (n > 0) {
+			digit = n % 10;
+			num += Math.pow(digit, count);
+			n = n / 10;
 		}
-		System.out.println(num==temp);
+		System.out.println(num == temp);
 	}
 }
