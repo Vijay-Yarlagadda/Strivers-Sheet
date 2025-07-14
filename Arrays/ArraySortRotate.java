18086a6d2089ea3ab1277017a69f6505789d40af
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-14 21:44:40 +0530

    Arrays Sort

---
+++ Arrays/ArraySortRotate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class ArraySortRotate {
+    public boolean check(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ArraySortRotate sol = new ArraySortRotate();
+        boolean result = sol.check(arr);
+        System.out.println("Check result: " + result);
+    }
+}
