3af6fc87d83d8ee34be6253a428d92749a10c15e
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-14 22:42:48 +0530

    Removing Duplicates

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i : nums)
+            set.add(i);
+        int k = 0;
+        for (int val : set)
+            nums[k++] = val;
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        RemoveDuplicates sol = new RemoveDuplicates();
+        int k = sol.removeDuplicates(nums);
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
