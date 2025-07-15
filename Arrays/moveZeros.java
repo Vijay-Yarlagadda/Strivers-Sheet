efd6d84764825d86320ca3b06482066ba4c74abc
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-15 22:39:30 +0530

    move zeros to end

---
+++ Arrays/moveZeros.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class moveZeros {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        moveZeros mz = new moveZeros();
+        mz.moveZeroes(arr);
+        System.out.print("After moving zeroes: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
