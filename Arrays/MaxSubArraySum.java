330b02c68c4faefe740651908b74954cc7fb67e6
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-07-23 21:30:28 +0530

    KADANE'S ALGO

---
+++ Arrays/MaxSubArraySum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class MaxSubArraySum {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max)
+                max = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        MaxSubArraySum obj = new MaxSubArraySum();
+        System.out.println(obj.maxSubArray(nums));
+    }
+}
