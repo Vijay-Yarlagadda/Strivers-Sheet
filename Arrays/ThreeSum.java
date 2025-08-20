e5d6040590e60ac66baff8341159cbed1aabb307
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-08-20 21:33:27 +0530

    Three Sum

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -3,19 +3,31 @@ import java.util.*;
 class ThreeSum {
 
     public static List<List<Integer>> threeSum(int nums[]) {
-        Set<List<Integer>> st = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                        temp.sort(null);
-                        st.add(temp);
-                    }
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    list.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
         }
-        List<List<Integer>> list = new ArrayList<>(st);
         return list;
     }
 
