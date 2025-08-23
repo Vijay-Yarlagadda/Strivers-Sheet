6c3827c1cb8bbc1018248ca4bdac40c3420ce693
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-08-23 22:36:01 +0530

    MaxSubArrSumZero

---
+++ Arrays/MaxSubArrSumZero.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class MaxSubArrSumZero {
+    public static int maxLen(int[] arr) {
+        // Your code goes here
+        int sum = 0, count = 0;
+        int start = 0, max = 0;
+        for (int i = 0; start < arr.length; i++) {
+            sum += arr[i];
+            count++;
+            if (sum == 0 && i != arr.length - 1) {
+                max = Math.max(max, count);
+                continue;
+            }
+            if (sum == 0 && i == arr.length - 1) {
+                max = Math.max(max, count);
+                count = 0;
+                start++;
+                i = start - 1;
+            }
+            if (i == arr.length - 1 && sum != 0) {
+                start++;
+                i = start - 1;
+                sum = 0;
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter arrray length: ");
+        int n = sc.nextInt();
+        System.out.println("Enter array elements: ");
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int result = maxLen(a);
+        System.out.println("Max sub array sum zero is: " + result);
+        sc.close();
+    }
+}
