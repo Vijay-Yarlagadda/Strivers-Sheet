2964bf4c850051c5de14e7582befc95344c9065f
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-02 22:14:18 +0530

    GDC

--- Digits-Concept/Gdc.java
+++ Digits-Concept/Gdc.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
-class Gdc{
+class Gdc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	int n1,n2;
-	System.out.println("Enter two numbers: ");
-	n1=sc.nextInt();
-	n2=sc.nextInt();
-        int i,temp=0;
-            for(i=1;i<=Math.min(n1,n2);i++){
-                if(n1%i==0 && n2%i==0){
-                    temp=i;
-                }
+        int n1, n2;
+        System.out.println("Enter two numbers: ");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        sc.close();
+        int i, temp = 0;
+        for (i = 1; i <= Math.min(n1, n2); i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                temp = i;
             }
-	System.out.println("GDC:" );
-		System.out.println(temp);
+        }
+        System.out.println("GDC:");
+        System.out.println(temp);
     }
 }
