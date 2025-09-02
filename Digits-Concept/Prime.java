9469df96f4096db5e57e1aa3cd757066db26f925
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-02 22:15:07 +0530

    prime number

--- Digits-Concept/Prime.java
+++ Digits-Concept/Prime.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n,i;
+        int n, i;
         System.out.print("Enter a number: ");
-        n=sc.nextInt();
-        int count=0;
-            for(i=1;i<=n;i++){
-            if(n%i==0){
+        n = sc.nextInt();
+        sc.close();
+        int count = 0;
+        for (i = 1; i <= n; i++) {
+            if (n % i == 0) {
                 count++;
             }
-          }
-	System.out.println(count==2);
+        }
+        System.out.println(count == 2);
     }
 }
