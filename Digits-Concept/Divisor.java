1ee4d038360388b0f34ecbc0b96743f7dc159e84
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-02 22:13:47 +0530

    Divisor

--- Digits-Concept/Divisor.java
+++ Digits-Concept/Divisor.java
@@ -1,24 +1,25 @@
 import java.util.*;
 
 class Divisor {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number: ");
-		int n = sc.nextInt();
-		int i;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(i=1;i<=n;i++){
-                    if(n%i==0){
-                        list.add(i);
-                    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        sc.close();
+        int i;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
             }
-            //into Array
-            int [] result=new int[list.size()];
-            for(i=0;i<list.size();i++){
-                result[i]=list.get(i);
-            }
-            for(i=0;i<list.size();i++){
-			System.out.print(result[i]);
-		}
-	}
+        }
+        // into Array
+        int[] result = new int[list.size()];
+        for (i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        for (i = 0; i < list.size(); i++) {
+            System.out.print(result[i]);
+        }
+    }
 }
