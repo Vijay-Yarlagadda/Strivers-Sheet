75e20a6d414ead6f5488865ba143c55eebbd4a6b
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-02 22:12:29 +0530

    Count no of digits

--- Digits-Concept/CountDigits.java
+++ Digits-Concept/CountDigits.java
@@ -5,12 +5,13 @@ class CountDigits {
         Scanner sc = new Scanner(System.in);
         int n;
         System.out.print("Enter a number: ");
-        n=sc.nextInt();
-        int count=0;
-            while(n!=0){
-                count++;
-                n=n/10;
-            }
-            System.out.println(count);
+        n = sc.nextInt();
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n / 10;
+        }
+        System.out.println(count);
+        sc.close();
     }
 }
