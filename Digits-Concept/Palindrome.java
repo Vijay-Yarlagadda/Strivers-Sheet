97123614d3f6d3b9e97753e143c886a27faab27c
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-02 22:14:43 +0530

    Palindrome

--- Digits-Concept/Palindrome.java
+++ Digits-Concept/Palindrome.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
-class Palindrome{
+class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
         System.out.print("Enter a number: ");
-        n=sc.nextInt();
-        int rev=0,digit,temp=n;
-            while(n!=0){
-                digit=n%10;
-                rev=rev*10+digit;
-                n=n/10;
-            }
-            System.out.println(rev==temp);
+        n = sc.nextInt();
+        sc.close();
+        int rev = 0, digit, temp = n;
+        while (n != 0) {
+            digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+        System.out.println(rev == temp);
     }
 }
