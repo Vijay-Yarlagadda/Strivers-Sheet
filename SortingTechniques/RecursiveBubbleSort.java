fd330fcc3989c3c5d5a43fa99acb23b10e785e7c
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-06 22:52:49 +0530

    Bubble sort rec

--- SortingTechniques/RecursiveBubbleSort.java
+++ SortingTechniques/RecursiveBubbleSort.java
@@ -23,6 +23,7 @@ class RecursiveBubbleSort {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array length: ");
         int n = sc.nextInt();
         int a[] = new int[n];
         for (int i = 0; i < n; i++) {
