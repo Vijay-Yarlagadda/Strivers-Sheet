d0e9c951d550f8d72684c58cff6a0b00a209cc7d
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-08 21:49:48 +0530

    Merge Sort

--- SortingTechniques/MergeSorting.java
+++ SortingTechniques/MergeSorting.java
@@ -48,6 +48,7 @@ public class MergeSorting {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
         int n = sc.nextInt();
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
