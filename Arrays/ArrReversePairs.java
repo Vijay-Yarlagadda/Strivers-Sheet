fd915248c5d80ac89884549ff8169437ac6c4fa6
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-09-09 21:31:41 +0530

    Reverse Pairs

--- Arrays/ArrReversePairs.java
+++ Arrays/ArrReversePairs.java
@@ -1,16 +1,57 @@
 import java.util.*;
 
 class ArrReversePairs {
-    public static int reversePairs(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > 2 * nums[j] && nums[i] > nums[j]) {
-                    count++;
-                }
+    public static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                list.add(arr[left]);
+                left++;
+            } else {
+                list.add(arr[right]);
+                right++;
             }
         }
+        while (left <= mid) {
+            list.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            list.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = list.get(i - low);
+        }
+    }
+
+    public static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int count = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right])
+                right++;
+            count += (right - (mid + 1));
+        }
         return count;
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int count = 0;
+        if (low >= high)
+            return count;
+        int mid = (low + high) / 2;
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+        count += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return count;
+    }
+
+    public static int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
     }
 
     public static void main(String args[]) {
