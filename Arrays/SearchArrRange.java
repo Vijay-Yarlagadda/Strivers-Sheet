f255981ff6f7ba9dbc8258f75662782395c58555
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2025-12-01 10:05:17 +0530

    First & Last OCC

---
+++ Arrays/SearchArrRange.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class SearchArrRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[] { first, last };
+    }
+
+    public static int findFirst(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+            if (nums[mid] == target) {
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+    public static int findLast(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            if (nums[mid] == target) {
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+        System.out.println("Enter array elements: ");
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Enter target element: ");
+        int target = sc.nextInt();
+        int res[] = searchRange(a, target);
+        System.out.println("The starting and ending positions are: ");
+        for (int ele : res) {
+            System.out.print(ele + " ");
+        }
+        sc.close();
+    }
+}
